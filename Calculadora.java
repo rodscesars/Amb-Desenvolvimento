cb3adfda6757840a696003fd735cf1fe1e73feae
Rodrigo Mendes <rodrigocesarsm@gmail.com>
2022-06-07 19:55:01 -0300

    Fixed import in class Calculadora

--- Calculadora.java
+++ Calculadora.java
@@ -1,38 +1,40 @@
-public class Calculadora {
-    
-    public Calculadora(){}
+import java.lang.Math;
 
-    public double soma(double valor1, double valor2){
+public class Calculadora {
+
+    public Calculadora() {
+    }
+
+    public double soma(double valor1, double valor2) {
         return valor1 + valor2;
     }
 
-    public double subtracao(double valor1, double valor2){
+    public double subtracao(double valor1, double valor2) {
         return valor1 - valor2;
     }
 
-    public double divisao(double valor1, double valor2){
+    public double divisao(double valor1, double valor2) {
         return valor1 / valor2;
     }
 
-    public double multiplicacao(double valor1, double valor2){
+    public double multiplicacao(double valor1, double valor2) {
         return valor1 * valor2;
     }
 
-    public double exponencial(double base, double expoente){
+    public double exponencial(double base, double expoente) {
         return Math.pow(base, expoente);
     }
 
-    public double raizQuadrada(double valor1){
+    public double raizQuadrada(double valor1) {
         return Math.sqrt(valor1);
     }
 
-    public double piso(double valor1){
+    public double piso(double valor1) {
         return Math.floor(valor1);
     }
 
-    public double teto(double valor1){
+    public double teto(double valor1) {
         return Math.ceil(valor1);
     }
-
 
 }
