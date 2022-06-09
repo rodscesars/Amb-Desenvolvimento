3328272f4c511356007124f45fcfcbeab20f3bbd
Isabely de Oliveira <isabelyy2002@gmail.com>
2022-06-09 15:18:44 -0300

    Add classe calculo velocidade/distância e fatorial

---
+++ CalculoFatorial.java
@@ -0,0 +1,24 @@
+import java.util.scanner;
+
+public class Fatorial {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+
+        Scanner scanner = new Scanner(System.in);
+
+        double x = numero;
+
+        System.out.println("Digite o número: ");
+        nome = scanner.nextLine();
+        scanner.nextLine();
+
+        double f = x;
+
+        while(x > 1){
+            f= f*(x-1);
+            x--;
+        }
+        
+        System.out.println("O Fatorial do número "+ numero + "\né " + f );
+        scanner.close();
+}
